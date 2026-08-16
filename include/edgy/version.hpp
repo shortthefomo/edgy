c8428a77a5b787beef69f901c20a99849b44bfb9
shortthefomo <508629+shortthefomo@users.noreply.github.com>
2026-08-16 11:20:00 -0400

    Set version to 0.1.2-b0
    Follow the 0.1.1 tag so further develop work is a prerelease.

--- include/edgy/version.hpp
+++ include/edgy/version.hpp
@@ -6,7 +6,7 @@ namespace edgy {
 
 // SemVer. Edit this for each tagged release, same as rippled BuildInfo.
 // After a tag, bump develop to the next -bN prerelease (e.g. 0.1.2-b0).
-inline constexpr char const* kVersionBase = "0.1.1";
+inline constexpr char const* kVersionBase = "0.1.2-b0";
 
 [[nodiscard]] std::string
 versionString();
